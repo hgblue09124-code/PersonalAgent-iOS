011cb399b1322603ea08fb37ff81e50bdf7c3309
hgblue09124-code <ftmt6fhyyd@privaterelay.appleid.com>
2026-09-26 13:16:15 +0700

    fix: restore composition dependency on local model storage

--- Package.swift
+++ Package.swift
@@ -164,6 +164,7 @@ let package = Package(
                 "PAEvents",
                 "PAProviders",
                 "PAProvidersLocal",
+                "PAStorageModels",
 
                 "PASecurity",
                 "PAModules",
